f57455162687a70ac19d3eb3cdcb0160b0d718b2
BetaFloof <107772196+BetaFloof@users.noreply.github.com>
2023-04-17 08:59:44 +0200

    Added board install

--- build.sh
+++ build.sh
@@ -1,14 +1,12 @@
 # A small script using the arduino-cli to compile a .ino file to a .uf2 file for the rp2040
-echo "mkdir build"
+
+# Install board
+arduino-cli core update-index
+arduino-cli core install arduino:mbed_rp2040
+
+# Build
 mkdir build
-
-echo "arduino-cli compile --fqbn arduino:mbed_rp2040:pico --output-dir ./build ."
 arduino-cli compile --fqbn arduino:mbed_rp2040:pico --output-dir ./build .
-
-echo "mv ./build/*.ino.uf2 ."
 mv ./build/*.ino.uf2 .
-
-echo "rm -rf ./build"
 rm -rf ./build
-
 echo "Done"
